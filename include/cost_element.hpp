a3d983a3c658ba6dbd316e8689cf7043f2998ea0
Juan A. Rico <jarico@unex.es>
2023-03-31 09:59:57 +0200

    CostElement: new abstract class to introduce Comoputation. Not yet active.

---
+++ include/cost_element.hpp
@@ -0,0 +1,30 @@
+//
+//  cost_element.hpp
+//
+//  Created by jarico on 31/3/23.
+//  Copyright © 2016 Juan A. Rico. All rights reserved.
+//
+//   Represents a Cost imputable element (Transmission or Computation).
+//
+
+#ifndef cost_element_hpp
+#define cost_element_hpp
+
+
+#include <iostream>
+
+// Class represents a cost imputable element (Transmission or Computation)
+
+class CostElement {
+      
+public:
+   
+             CostElement ();
+            ~CostElement ();
+      
+//   double    getCost     ();  // Cost of the blocks in computation
+//   long      getBytes    (double t);
+   
+};
+
+#endif /* cost_element_hpp */
---
+++ src/cost_element.cpp
@@ -0,0 +1,38 @@
+//
+//  cost_element.cpp
+//
+//  Created by jarico on 31/3/23.
+//  Copyright © 2016 Juan A. Rico. All rights reserved.
+//
+
+#include "cost_element.hpp"
+
+
+CostElement::CostElement () {
+}
+
+
+CostElement::~CostElement () {
+    
+}
+
+/*
+double Computation::getCost () {
+    
+    double t = 0.0;
+    
+    t = this->params->getTime(m, 1, 0);  // TODO: eliminar channel
+    
+    return t;
+}
+
+
+long Computation::getBytes (double t) {
+  
+    long b = 0;
+    
+    b = this->params->getBytes(t, 1, 0);  // TODO: eliminar channel
+    
+    return b;
+}
+*/
